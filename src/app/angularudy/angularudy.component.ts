096238ba4b9bf49f37093a558ff2fe8c0cc68d5d
Joel Dachelet <j.dachelet@thomas-piron.eu>
2022-10-17 16:07:09 +0200

    ruuuuudy

--- src/app/angularudy/angularudy.component.ts
+++ src/app/angularudy/angularudy.component.ts
@@ -8,6 +8,7 @@ import { NgxSpinnerService } from "ngx-spinner";
 })
 export class AngularudyComponent implements OnInit {
 
+  public rudyisWinning = false;
   private numb = 0;
   public text = "";
   constructor(private spinner: NgxSpinnerService) { }
@@ -24,11 +25,15 @@ export class AngularudyComponent implements OnInit {
 
   click(){
     this.numb++;
-    this.text += "Yeah Mich!";
+    this.text += " Yeah Mich! ";
 
-    if(this.numb === 5)
-    {this.text = "Rudy win the game !"
-  this.numb = 0;}
+    if(this.numb === 5){
+      this.text = "";
+      this.numb = 0;
+      this.rudyisWinning = !this.rudyisWinning;
+    }
+
+    if(this.rudyisWinning)  this.text = " Rudy win the game ! (" + this.numb + "/5)" 
   }
 
 }
