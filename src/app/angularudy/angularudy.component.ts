9abf26773b3c1217b63dcea5def5b13d4b6b11af
CedricH <c.havenne@thomas-piron.eu>
2022-10-17 15:47:12 +0200

    added one package

--- src/app/angularudy/angularudy.component.ts
+++ src/app/angularudy/angularudy.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from "ngx-spinner";
 
 @Component({
   selector: 'app-angularudy',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AngularudyComponent implements OnInit {
 
-  constructor() { }
+  constructor(private spinner: NgxSpinnerService) { }
 
   ngOnInit(): void {
+    /** spinner starts on init */
+    this.spinner.show();
+
+    setTimeout(() => {
+      /** spinner ends after 5 seconds */
+      this.spinner.hide();
+    }, 5000);
   }
 
 }
